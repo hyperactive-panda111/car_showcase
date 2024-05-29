976968bff571121bdeb2a395e708aeb9ad308c22
Yaw Amo Mensah <amomensah300@gmail.com>
2024-05-29 21:21:33 +0100

    adding fixes for .env variable

--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export async function fetchCars( filters: FilterProps) {
     model, limit, fuel} = filters;
 
     const headerss = {
-        'X-RapidAPI-Key': String(process.env.CAR_KEY),
+        'X-RapidAPI-Key': process.env.CAR_KEY as string,
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
     }
 
