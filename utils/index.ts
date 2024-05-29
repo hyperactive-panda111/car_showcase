05fdfaca124ee3ca125f0d4f015a6863082610ff
Yaw Amo Mensah <amomensah300@gmail.com>
2024-05-29 20:56:44 +0100

    fixing exposed API key

--- utils/index.ts
+++ utils/index.ts
@@ -4,14 +4,14 @@ export async function fetchCars( filters: FilterProps) {
     const {manufacturer, year, 
     model, limit, fuel} = filters;
 
-    const headers = {
-        'X-RapidAPI-Key': 'c4fe0a3704mshd34d8206cb4cbdep1d2c2ajsnee7343c8ce38',
+    const headerss = {
+        'X-RapidAPI-Key': String(process.env.CAR_KEY),
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
     }
 
     const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?
     make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
-        headers: headers,
+        headers: headerss,
     });
 
     const result = await response.json();
